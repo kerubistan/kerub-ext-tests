7fe76c0c4b281aed1973b51243b9e7485c0ab167
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2018-10-01 20:59:52 +0200

    new version of opensuse_42 image
    Signed-off-by: Laszlo Hornyak <laszlo.hornyak@gmail.com>

--- src/test/groovy/com/github/kerubistan/kerub/it/blocks/virt/VirtDefs.groovy
+++ src/test/groovy/com/github/kerubistan/kerub/it/blocks/virt/VirtDefs.groovy
@@ -24,7 +24,7 @@ class VirtDefs {
 
 	static disks = [
 			"centos_7" : new Tuple("kerub-centos-7-all-5.qcow2", 9),
-			"opensuse_42": new Tuple("kerub-openSUSE-42-all-1.qcow2", 13)
+			"opensuse_42": new Tuple("kerub-openSUSE-42-all-2.qcow2", 13)
 	]
 
 	Map<String, UUID> vms = new HashMap()
